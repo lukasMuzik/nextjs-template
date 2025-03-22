592a46070c47e2bfbd31d06957d671a96686c563
Lukáš Mužík <lukasmuzik.dev@gmail.com>
2025-03-22 12:24:46 +0100

    chore: add drizzle/postgres

---
+++ src/db/schema/schema.ts
@@ -0,0 +1,6 @@
+// export const usersTable = pgTable("users", {
+//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+//   name: varchar({ length: 255 }).notNull(),
+//   age: integer().notNull(),
+//   email: varchar({ length: 255 }).notNull().unique(),
+// });
